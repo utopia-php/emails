fd80871913304a0bf967ab0ce5fe96bf632f2a24
Matej Bačo <matejbaco2000@gmail.com>
2026-05-20 14:52:46 +0200

    Update tests for new protonmail behaviour

--- tests/Canonicals/Providers/ProtonmailTest.php
+++ tests/Canonicals/Providers/ProtonmailTest.php
@@ -19,6 +19,7 @@ class ProtonmailTest extends TestCase
         $this->assertTrue($this->provider->supports('protonmail.com'));
         $this->assertTrue($this->provider->supports('proton.me'));
         $this->assertTrue($this->provider->supports('pm.me'));
+        $this->assertTrue($this->provider->supports('protonmail.ch'));
         $this->assertFalse($this->provider->supports('gmail.com'));
         $this->assertFalse($this->provider->supports('outlook.com'));
         $this->assertFalse($this->provider->supports('example.com'));
@@ -27,31 +28,33 @@ class ProtonmailTest extends TestCase
     public function test_get_canonical(): void
     {
         $testCases = [
-            // ProtonMail preserves all characters (no subaddress or dot removal)
+            // ProtonMail removes plus addressing but preserves dots
             ['user.name', 'protonmail.com', 'user.name', 'protonmail.com'],
-            ['user.name+tag', 'protonmail.com', 'user.name+tag', 'protonmail.com'],
-            ['user.name+spam', 'protonmail.com', 'user.name+spam', 'protonmail.com'],
-            ['user.name+newsletter', 'protonmail.com', 'user.name+newsletter', 'protonmail.com'],
-            ['user.name+work', 'protonmail.com', 'user.name+work', 'protonmail.com'],
-            ['user.name+personal', 'protonmail.com', 'user.name+personal', 'protonmail.com'],
-            ['user.name+test123', 'protonmail.com', 'user.name+test123', 'protonmail.com'],
-            ['user.name+anything', 'protonmail.com', 'user.name+anything', 'protonmail.com'],
-            ['user.name+verylongtag', 'protonmail.com', 'user.name+verylongtag', 'protonmail.com'],
-            ['user.name+tag.with.dots', 'protonmail.com', 'user.name+tag.with.dots', 'protonmail.com'],
-            ['user.name+tag-with-hyphens', 'protonmail.com', 'user.name+tag-with-hyphens', 'protonmail.com'],
-            ['user.name+tag_with_underscores', 'protonmail.com', 'user.name+tag_with_underscores', 'protonmail.com'],
-            ['user.name+tag123', 'protonmail.com', 'user.name+tag123', 'protonmail.com'],
+            ['user.name+tag', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+spam', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+newsletter', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+work', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+personal', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+test123', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+anything', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+verylongtag', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+tag.with.dots', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+tag-with-hyphens', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+tag_with_underscores', 'protonmail.com', 'user.name', 'protonmail.com'],
+            ['user.name+tag123', 'protonmail.com', 'user.name', 'protonmail.com'],
             ['u.s.e.r.n.a.m.e', 'protonmail.com', 'u.s.e.r.n.a.m.e', 'protonmail.com'],
-            ['u.s.e.r.n.a.m.e+tag', 'protonmail.com', 'u.s.e.r.n.a.m.e+tag', 'protonmail.com'],
-            ['user+', 'protonmail.com', 'user+', 'protonmail.com'],
+            ['u.s.e.r.n.a.m.e+tag', 'protonmail.com', 'u.s.e.r.n.a.m.e', 'protonmail.com'],
+            ['user+', 'protonmail.com', 'user', 'protonmail.com'],
             ['user.', 'protonmail.com', 'user.', 'protonmail.com'],
             ['.user', 'protonmail.com', '.user', 'protonmail.com'],
             ['user..name', 'protonmail.com', 'user..name', 'protonmail.com'],
-            // Other ProtonMail domains
-            ['user.name+tag', 'proton.me', 'user.name+tag', 'protonmail.com'],
-            ['user.name+tag', 'pm.me', 'user.name+tag', 'protonmail.com'],
-            ['user.name', 'proton.me', 'user.name', 'protonmail.com'],
-            ['user.name', 'pm.me', 'user.name', 'protonmail.com'],
+            // Other ProtonMail domains (kept as canonical, not aliases)
+            ['user.name+tag', 'proton.me', 'user.name', 'proton.me'],
+            ['user.name+tag', 'pm.me', 'user.name', 'pm.me'],
+            ['user.name', 'proton.me', 'user.name', 'proton.me'],
+            ['user.name', 'pm.me', 'user.name', 'pm.me'],
+            ['user.name', 'protonmail.ch', 'user.name', 'protonmail.ch'],
+            ['user.name+tag', 'protonmail.ch', 'user.name', 'protonmail.ch'],
         ];
 
         foreach ($testCases as [$inputLocal, $inputDomain, $expectedLocal, $expectedDomain]) {
@@ -69,7 +72,7 @@ class ProtonmailTest extends TestCase
     public function test_get_supported_domains(): void
     {
         $domains = $this->provider->getSupportedDomains();
-        $expected = ['protonmail.com', 'proton.me', 'pm.me'];
+        $expected = ['protonmail.com', 'proton.me', 'pm.me', 'protonmail.ch'];
         $this->assertSame($expected, $domains);
     }
 }
